97af33fe264a41d9796d7617d0364a94af2b55a1
Victor Vianna <vitorvianna27@gmail.com>
2025-10-24 21:03:03 -0300

    :feat:feat: Gateway Implementation

---
+++ gateway-module/src/test/java/com/infnet/gatewaymodule/GatewayModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infnet.gatewaymodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
